a92b561eb26fa5e37de37c105984117870bcfa24
Gustavo Reichelt Emmmel <gremmel@gmail.com>
2017-02-16 21:44:02 -0200

    aula 3

--- aula0206/routes/users.js
+++ aula0206/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  request('https://randomuser.me/api/', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.end(body);
+    }
+  }
+)});
 
 module.exports = router;
